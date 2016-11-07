68c928b20a7f237e722ebc0205776dcdb27a1c41
Rafael Santos <rsanthos@gmail.com>
2016-11-07 12:05:40 -0200

    Property and Two-way data binding

--- app/app.component.js
+++ app/app.component.js
@@ -11,12 +11,15 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
-        this.title = "Hello World";
+        this.title = {
+            name: 'Hello World',
+            description: 'Minha descricao'
+        };
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n    <h1>{{ title }}</h1>\n    <input  value=\"{{ title }}\" />\n    "
+            template: "\n    <h1>{{ title.name }}</h1>\n    <input [value]=\"title.name\"/>\n    <input [(ngModel)]=\"title.description\"/>\n    {{ title.name }}\n    {{ title.description }}\n    "
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -3,11 +3,17 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
-    <h1>{{ title }}</h1>
-    <input  value="{{ title }}" />
+    <h1>{{ title.name }}</h1>
+    <input [value]="title.name"/>
+    <input [(ngModel)]="title.description"/>
+    {{ title.name }}
+    {{ title.description }}
     `
 })
 
 export class AppComponent{
-    title = "Hello World";
+    title = {
+        name: 'Hello World',
+        description: 'Minha descricao'
+    };
 }
