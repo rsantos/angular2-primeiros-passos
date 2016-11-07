a8f04d2bf271e692ee50da839c27fb1b1d6bcdc7
Rafael Santos <rsanthos@gmail.com>
2016-11-07 11:15:41 -0200

    com interpolação

--- app/app.component.js
+++ app/app.component.js
@@ -11,11 +11,12 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = "Hello World";
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "\n    <h1>Hello World</h1>\n    "
+            template: "\n    <h1>{{ title }}</h1>\n    <input  value=\"{{ title }}\" />\n    "
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,11 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
-    <h1>Hello World</h1>
+    <h1>{{ title }}</h1>
+    <input  value="{{ title }}" />
     `
 })
 
 export class AppComponent{
-
+    title = "Hello World";
 }
